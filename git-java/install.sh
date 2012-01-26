acdacea9de206b29c76c9705dbd507ce519b0f3f
Paul Phillips <paulp@improving.org>
2012-01-25 18:33:09 -0800

    More sensible git setup.

--- git-java/install.sh
+++ git-java/install.sh
@@ -1,42 +1,82 @@
 #!/usr/bin/env bash
 #
-# !!! This will modify (or create) your ~/.gitconfig and ~/.gitattributes.
-# It's a pure append operation.
+# !!! This will modify (or create) your $HOME/.gitconfig and $HOME/.gitattributes.
+# It's adding this to your core.attributes:
+#
+#   *.class diff=textconv-javap
+#   *.jar   diff=textconv-jar
+#
+# And then the config to make use of it to .gitconfig.
 
 gitJavaDir="$(cd $(dirname $BASH_SOURCE) && pwd)"
+defaultAttributes="$HOME/.gitattributes"
 
-gitconfigData () {
-  cat <<EOM
-[diff "javap"]
-	textconv = $gitJavaDir/textconv-javap
-	cachetextconv = true
-
-[diff "jar-toc"]
-  textconv = $gitJavaDir/textconv-jar
-  cachetextconv = true
-EOM
+die () {
+  echo "$@" && exit 1
+}
+gitConfig () {
+  git config --global "$@"
+}
+gitConfigAdd () {
+  key="$1"
+  value="$2"
+  gitConfig --add "$key" "$value" && echo "[created] config $key = $value"
+}
+gitConfigGet () {
+  gitConfig --get "$@"
 }
 
-gitAttributesData () {
-  cat <<EOM
-*.class diff=javap
-*.jar   diff=jar-toc
-EOM
+gitConfigAddIfAbsent () {
+  if gitConfigGet "$1" >/dev/null; then
+    echo "[exists]    config  $1 = $(gitConfigGet $1)"
+  else
+    gitConfigAdd "$@"
+  fi
 }
 
-[[ -f ~/.gitconfig     ]] || touch ~/.gitconfig
-[[ -f ~/.gitattributes ]] || touch ~/.gitattributes
+gitAttributesFile () {
+  local file=$(gitConfigGet core.attributesfile)
+  [[ -n "$file" ]] || {
+    gitConfigAdd core.attributesfile "$defaultAttributes"
+    file=$(gitConfigGet core.attributesfile)
+  }
+  if [[ -f "$file" ]]; then
+    echo "$file"
+  else
+    eval "touch $file && ls -1 $file"
+  fi
+}
+    
+attributesFile="$(gitAttributesFile)"
+[[ -f "$attributesFile" ]] || {
+  touch "$attributesFile" && echo "[created] attributes file $attributesFile"
+}
 
-if grep -q '\[diff "javap"\]' ~/.gitconfig; then
-  echo "~/.gitconfig already contains a javap entry: doing nothing."
-else
-  gitconfigData >> ~/.gitconfig
-  echo "Updated ~/.gitconfig with git-java configuration."
-fi
+gitAttributeAdd () {
+  local ext="$1"
+  local handler="$2"
+  
+  [[ -f "$attributesFile" ]] && {
+    existing="$(egrep "=${handler}\b" "$attributesFile")"
+    
+    if [[ -n "$existing" ]]; then
+      echo "[exists] attribute  $existing"
+    else
+      line="*.${ext} diff=${handler}"
+      echo "$line" >>"$attributesFile"
+      echo "[created] attribute $line"
+    fi
+  }
+}
 
-if grep -q 'diff=javap' ~/.gitattributes; then
-  echo "~/.gitattributes already contains a javap entry: doing nothing."
-else
-  gitAttributesData >> ~/.gitattributes
-  echo "Updated ~/.gitattributes with git-java attributes."
-fi
+gitAttributeAdd class javap
+gitAttributeAdd jar jar-toc
+
+gitConfigAddIfAbsent diff.javap.textconv "$gitJavaDir/textconv-javap"
+gitConfigAddIfAbsent diff.javap.cachetextconv true
+
+gitConfigAddIfAbsent diff.jar-toc.textconv "$gitJavaDir/textconv-jar"
+gitConfigAddIfAbsent diff.jar-toc.cachetextconv true
+
+echo ""
+echo "git-java setup complete.  Try running git-java/demo.sh"
